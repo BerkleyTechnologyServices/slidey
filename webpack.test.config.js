f46408a4150947b71bd65d168185822ab2cbcae7
Nick Woodward <nick@salte.io>
2018-02-25 20:53:06 -0600

    fix: upgrade to webpack 4.0.0 (#65)

--- webpack.test.config.js
+++ webpack.test.config.js
@@ -4,11 +4,6 @@ module.exports = {
   },
   module: {
     rules: [{
-      enforce: 'pre',
-      test: /\.js$/,
-      include: /tests/,
-      loader: 'eslint-loader'
-    }, {
       test: /\.js$/,
       exclude: /node_modules/,
       loader: 'babel-loader'
@@ -22,5 +17,8 @@ module.exports = {
       loader: 'raw-loader'
     }]
   },
-  devtool: 'inline-source-map'
+  devtool: 'inline-source-map',
+  optimization: {
+    minimize: false
+  }
 };
