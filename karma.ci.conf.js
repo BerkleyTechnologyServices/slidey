f4993657ed61409546eca8057439de684c2bebab
Ceci <admin@cecilias.me>
2018-11-13 18:07:41 -0600

    chore: don't test in Safari 10 due to a karma bug

--- karma.ci.conf.js
+++ karma.ci.conf.js
@@ -24,11 +24,12 @@ module.exports = function(config) {
       browserName: 'internet explorer',
       version: '11'
     },
-    Safari10: {
-      base: 'SauceLabs',
-      browserName: 'safari',
-      version: '10'
-    },
+    // TODO: Enable this once https://github.com/karma-runner/karma/issues/3198 is resolved
+    // Safari10: {
+    //   base: 'SauceLabs',
+    //   browserName: 'safari',
+    //   version: '10'
+    // },
     Safari9: {
       base: 'SauceLabs',
       browserName: 'safari',
